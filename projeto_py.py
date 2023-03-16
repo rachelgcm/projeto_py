138a9baa543a0cec8e06279d6a1bb5ffdf62b9fb
unknown <raquelcarvalhogc@gmail.com>
2023-03-16 16:50:23 -0300

    coloquei linhas de programação para calcular a idade com base no ano do nascimento

---
+++ projeto_py.py
@@ -0,0 +1,8 @@
+nascimento = 2006
+nome = 'Raquel'
+anoAtual = 2023
+def calculoidade(nascimento,anoAtual):
+    idade= anoAtual - nascimento
+    return idade
+idade = calculoidade(nascimento,anoAtual)
+print('idade é', idade)
